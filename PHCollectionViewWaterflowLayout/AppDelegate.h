e08f9d86a6a435247083b4ce04442255b9aac985
Phineas-z <mcgradylue@gmail.com>
2012-11-12 11:59:35 +0800

    Initial Commit

---
+++ PHCollectionViewWaterflowLayout/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PHCollectionViewWaterflowLayout
+//
+//  Created by Appublisher  on 12-11-12.
+//  Copyright (c) 2012年 Appublisher . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PHCollectionViewWaterflowLayout/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PHCollectionViewWaterflowLayout
+//
+//  Created by Appublisher  on 12-11-12.
+//  Copyright (c) 2012年 Appublisher . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
